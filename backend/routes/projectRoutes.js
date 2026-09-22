ecfe467143feb5764ae9ca5b90137cead6777a45
Kailash <kalviumstudent10@gmail.com>
2026-09-22 11:12:47 +0530

    feat: add project update and delete workflow

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { getProjects, createProject, updateProject } = require("../controllers/projectController");
+const { getProjects, createProject, updateProject, deleteProject } = require("../controllers/projectController");
 
 const router = express.Router();
 router.route("/").get(getProjects).post(createProject);
-router.route("/:id").put(updateProject);
+router.route("/:id").put(updateProject).delete(deleteProject);
 
 module.exports = router;
